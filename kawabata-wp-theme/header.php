6142fa0bd342fd6c8e4b7cd87c4fbfc6751e540b
kodamamnma <k4923590@kadai.jp>
2026-06-19 19:40:54 +0900

    618 gmail修正

--- kawabata-wp-theme/header.php
+++ kawabata-wp-theme/header.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="google-adsense-account" content="ca-pub-2831019593005123">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
